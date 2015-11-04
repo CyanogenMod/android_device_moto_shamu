0b6f2658b2be973ee9ae653eeb5928af7541b082
Steve Kondik <steve@cyngn.com>
2015-11-04 00:51:06 -0500

    power: Update for PerformanceManager changes
    Change-Id: I3336e3ba0751f4d7a6c0e04546b7f89aaa747e49
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- power/power_shamu.c
+++ power/power_shamu.c
@@ -251,7 +251,7 @@ static void sysfs_write(char *path, char *s)
     close(fd);
 }
 
-static void set_feature(struct power_module *module, feature_t feature, int state)
+static void set_feature(struct power_module *module __unused, feature_t feature, int state)
 {
     switch (feature) {
     case POWER_FEATURE_DOUBLE_TAP_TO_WAKE:
@@ -261,6 +261,14 @@ static void set_feature(struct power_module *module, feature_t feature, int stat
         ALOGW("Error setting the feature, it doesn't exist %d\n", feature);
         break;
     }
+}
+
+int get_feature(struct power_module *module __unused, feature_t feature)
+{
+    if (feature == POWER_FEATURE_SUPPORTED_PROFILES) {
+        return 3;
+    }
+    return -1;
 }
 
 static void power_hint( __attribute__((unused)) struct power_module *module,
@@ -307,4 +315,5 @@ struct power_module HAL_MODULE_INFO_SYM = {
     .setInteractive = power_set_interactive,
     .powerHint = power_hint,
     .setFeature = set_feature,
+    .getFeature = get_feature
 };
