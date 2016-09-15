4d14750de3a5ffdceb04afcab02ac71245a44051
Pranav Vashi <neobuddy89@gmail.com>
2016-09-15 18:59:42 +0530

    shamu: Set radio props
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -216,7 +216,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.no_wait_for_card=1 \
-    persist.radio.data_no_toggle=1
+    persist.radio.data_no_toggle=1 \
+    persist.radio.sib16_support=1 \
+    persist.data.qmi.adb_logmask=0 \
+    persist.radio.alt_mbn_name=tmo_alt.mbn
 
 # WiFi calling
 PRODUCT_PROPERTY_OVERRIDES += \
