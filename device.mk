ec392cd666c467eb5df0a1d91a5b1b046e3c3eb0
Pranav Vashi <neobuddy89@gmail.com>
2016-09-15 19:48:07 +0530

    shamu: Add props for drm service and facelock
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -162,7 +162,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.gapless.enabled=true \
     audio.offload.pcm.16bit.enable=true \
     audio.offload.pcm.24bit.enable=true \
-    ro.audio.monitorRotation=true
+    ro.audio.monitorRotation=true \
+    drm.service.enabled=true \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false
 
 # Enable enhanced offload features
 AUDIO_FEATURE_ENABLED_FLAC_OFFLOAD := true
