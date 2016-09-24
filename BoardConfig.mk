55d5f66000332f1183a4a75cb3c82a4f30ece1f3
Pranav Vashi <neobuddy89@gmail.com>
2016-09-24 12:34:59 +0530

    shamu: Add CMHW implementation
    Change-Id: I44fb7b4a9cc0ae82182bc1dd86007c1950e203a2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,6 +134,12 @@ BOARD_HAL_STATIC_LIBRARIES := libdumpstate.shamu
 
 USE_CLANG_PLATFORM_BUILD := true
 
+# CMHW
+BOARD_USES_CYANOGEN_HARDWARE := true
+BOARD_HARDWARE_CLASS := \
+    hardware/cyanogen/cmhw   \
+    device/moto/shamu/cmhw
+
 # Disable dex-preopt of prebuilts to save space.
 DONT_DEXPREOPT_PREBUILTS := true
 
